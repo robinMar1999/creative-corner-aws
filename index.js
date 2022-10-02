50e0201c2f8126a00e0a3c82f7172c2bef35941c
Robin <bpria@amazon.com>
2022-10-03 00:59:20 +0530

    change port to 80

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get("/contact", async (req,res) => {
     res.render("contact")
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.listen(port, ()=>{
     console.log(`Server started on port: ${port}`);
